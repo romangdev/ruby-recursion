ad0546b494fca3847e969ef937da31a8f02b4611
Roman Geathers <romangeathersdev@gmail.com>
2022-06-28 13:49:54 -0400

    Complete first version of recurive merge sort

--- recursion.rb
+++ recursion.rb
@@ -21,6 +21,45 @@ def fibs_rec(n, arr = [0, 1], i = 2)
   fibs_rec(n, arr << (arr[i - 2] + arr[i - 1]), i + 1)
 end
 
-print fibs(8)
-puts "\n"
 print fibs_rec(8)
+puts "\n"
+
+def merge_sort(arr, a = [])
+  if arr.length < 2
+    return arr
+  else
+    b = merge_sort(arr.slice(0, arr.length/2))
+    c = merge_sort(arr.slice(arr.length/2, arr.length))
+
+    i = 0
+    n = 0
+
+    while i < b.length && n < c.length
+      if b[i] < c[n]
+        a << b[i]
+        i += 1
+      else
+        a << c[n]
+        n += 1
+      end
+    end
+
+    if i >= b.length
+      until n >= c.length
+        a << c[n]
+        n += 1
+      end
+    else
+      until i >= b.length
+        a << b[i]
+        i += 1
+      end
+    end
+    a
+  end
+  a
+end
+
+arr = [8, 9, 2, 5, 3, 1, 4, 9]
+print merge_sort(arr)
+puts "\n"
